9a6e369c97552a63bbd71d9b9ae25b765345295c
romeo <romeoquist@gmail.com>
2024-04-18 15:47:51 -0500

    Initializing Project

---
+++ src/main/java/com/romeoh20/goalhub/GoalHubApplication.java
@@ -0,0 +1,13 @@
+package com.romeoh20.goalhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romeoh20/goalhub/GoalHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romeoh20.goalhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
