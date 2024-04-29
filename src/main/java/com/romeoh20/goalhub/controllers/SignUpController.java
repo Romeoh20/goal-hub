f0d49aabc7c76e80a776c53ed79d632fc6315e0a
romeo <romeoquist@gmail.com>
2024-04-29 14:20:49 -0500

    added a MySQL dependency to the gradle build and created getUserFromSession method in Sign up controller

--- src/main/java/com/romeoh20/goalhub/controllers/SignUpController.java
+++ src/main/java/com/romeoh20/goalhub/controllers/SignUpController.java
@@ -1,15 +1,37 @@
 package com.romeoh20.goalhub.controllers;
 
+import com.romeoh20.goalhub.models.User;
 import com.romeoh20.goalhub.models.data.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
 
 @Controller
 public class SignUpController {
 
  @Autowired
-UserRepository userRepository;
-    
+ UserRepository userRepository;
+
+ private static final String userSessionKey = "user";
+
+ public User getUserFromSession (HttpSession session) {
+  Integer userId = (Integer) session.getAttribute(userSessionKey);
+
+  if(userId == null){
+   return null;
+  }
+
+  Optional <User> user = userRepository.findById(userId);
+
+  if(user.isEmpty()){
+   return null;
+  }
+
+  return user.get();
+ }
+
 
 
 
