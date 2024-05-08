87df0fe16fb270fddce7f51c4052938ba4bc82a5
romeo <romeoquist@gmail.com>
2024-05-08 15:56:17 -0500

    completed WebApplicationConfig

--- src/main/java/com/romeoh20/goalhub/WebApplicationConfig.java
+++ src/main/java/com/romeoh20/goalhub/WebApplicationConfig.java
@@ -2,6 +2,7 @@ package com.romeoh20.goalhub;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,6 +11,11 @@ public class WebApplicationConfig implements WebMvcConfigurer {
     @Bean
     public AuthenticationFilter authenticationFilter (){
         return new AuthenticationFilter();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(authenticationFilter());
     }
 
 
