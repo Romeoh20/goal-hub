a3ade40f7ce6987637bebd12cda547f45bc5d736
romeo <romeoquist@gmail.com>
2024-05-08 15:09:23 -0500

    Finished AuthenticationFilter

--- src/main/java/com/romeoh20/goalhub/AuthenticationFilter.java
+++ src/main/java/com/romeoh20/goalhub/AuthenticationFilter.java
@@ -1,13 +1,17 @@
 package com.romeoh20.goalhub;
 
 import com.romeoh20.goalhub.controllers.AuthenticationController;
+import com.romeoh20.goalhub.models.User;
 import com.romeoh20.goalhub.models.data.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class AuthenticationFilter implements HandlerInterceptor {
 
@@ -16,9 +20,33 @@ public class AuthenticationFilter implements HandlerInterceptor {
     @Autowired
     AuthenticationController authenticationController;
 
+    private static final List<String> whiteList = Arrays.asList("/login","/signup","/logout","/","css");
+
+    private static boolean isWhiteListed(String path){
+        for(String pathRoot : whiteList){
+            if(path.startsWith(pathRoot)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response
                             ,Object handler) throws IOException {
 
-        return true;
+        if(isWhiteListed(request.getRequestURI())){
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        if(user != null){
+            return true;
+        }
+
+        response.sendRedirect("/login");
+        return false;
     }
 }
